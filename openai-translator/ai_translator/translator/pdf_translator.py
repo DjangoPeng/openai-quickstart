0c8ed74caed37f831a8244dfa07cd24f96673d0a
DjangoPeng <pjt73651@gmail.com>
2023-07-30 18:39:51 +0800

    add initial version of openai-translator

---
+++ openai-translator/ai_translator/translator/pdf_translator.py
@@ -0,0 +1,26 @@
+from typing import Optional
+from model import Model
+from translator.pdf_parser import PDFParser
+from translator.writer import Writer
+from utils import LOG
+
+class PDFTranslator:
+    def __init__(self, model: Model):
+        self.model = model
+        self.pdf_parser = PDFParser()
+        self.writer = Writer()
+
+    def translate_pdf(self, pdf_file_path: str, file_format: str = 'PDF', target_language: str = '中文', output_file_path: str = None, pages: Optional[int] = None):
+        self.book = self.pdf_parser.parse_pdf(pdf_file_path, pages)
+
+        for page_idx, page in enumerate(self.book.pages):
+            for content_idx, content in enumerate(page.contents):
+                prompt = self.model.translate_prompt(content, target_language)
+                LOG.debug(prompt)
+                translation, status = self.model.make_request(prompt)
+                LOG.info(translation)
+                
+                # Update the content in self.book.pages directly
+                self.book.pages[page_idx].contents[content_idx].set_translation(translation, status)
+
+        self.writer.save_translated_book(self.book, output_file_path, file_format)
