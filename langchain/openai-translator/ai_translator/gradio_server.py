621e0bc65c1511bb822e297f34accd1d1220667b
lihengda <lihengda1992@126.com>
2024-05-13 19:18:37 +0800

    在 openai-translator gradio 图形化界面基础上，支持风格化翻译，如：小说、新闻稿、作家风格等。

--- langchain/openai-translator/ai_translator/gradio_server.py
+++ langchain/openai-translator/ai_translator/gradio_server.py
@@ -8,11 +8,11 @@ from utils import ArgumentParser, LOG
 from translator import PDFTranslator, TranslationConfig
 
 
-def translation(input_file, source_language, target_language):
-    LOG.debug(f"[翻译任务]\n源文件: {input_file.name}\n源语言: {source_language}\n目标语言: {target_language}")
+def translation(input_file, source_language, target_language, style):
+    LOG.debug(f"[翻译任务]\n源文件: {input_file.name}\n源语言: {source_language}\n目标语言: {target_language}\n风格：{style}")
 
     output_file_path = Translator.translate_pdf(
-        input_file.name, source_language=source_language, target_language=target_language)
+        input_file.name, source_language=source_language, target_language=target_language, style=style)
 
     return output_file_path
 
@@ -24,10 +24,12 @@ def launch_gradio():
         inputs=[
             gr.File(label="上传PDF文件"),
             gr.Textbox(label="源语言（默认：英文）", placeholder="English", value="English"),
-            gr.Textbox(label="目标语言（默认：中文）", placeholder="Chinese", value="Chinese")
+            gr.Textbox(label="目标语言（默认：中文）", placeholder="Chinese", value="Chinese"),
+            gr.Textbox(label="风格（默认：小说）", placeholder="Novel", value="novel")
         ],
         outputs=[
-            gr.File(label="下载翻译文件")
+            gr.File(label="下载翻译文件"),
+            # gr.Markdown(label="预览文件")
         ],
         allow_flagging="never"
     )
