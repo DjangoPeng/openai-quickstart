170fbfed40f251d15718586a871f650ed33df673
fanghua98 <imfanghua1998@gmail.com>
2024-07-14 13:42:38 +0800

    fix: optimize make_table_prompt function in model.py (#104)
    * fix: optimize make_table_prompt function in model.py
    
    - Modified the prompt used for GPT table text parsing to correctly translate tables and support DataFrame parsing"
    
    * 更新 langchian部分jupyter代码测试修改

--- openai-translator/ai_translator/model/model.py
+++ openai-translator/ai_translator/model/model.py
@@ -5,7 +5,8 @@ class Model:
         return f"翻译为{target_language}：{text}"
 
     def make_table_prompt(self, table: str, target_language: str) -> str:
-        return f"翻译为{target_language}，保持间距（空格，分隔符），以表格形式返回：\n{table}"
+        # return f"翻译为{target_language}，保持间距（空格，分隔符），以表格形式返回：\n{table}"
+        return f"翻译为{target_language}，以空格和换行符表示表格：\n{table}"
 
     def translate_prompt(self, content, target_language: str) -> str:
         if content.content_type == ContentType.TEXT:
